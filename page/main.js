4bec878a6687ed45cd4bd60ea4f46728a62fc4c4
NicolasPietroDEV <99685174+NicolasPietroDEV@users.noreply.github.com>
2022-03-15 15:27:09 -0300

    Versão 0.2 Beta (Com CSS)

--- page/main.js
+++ page/main.js
@@ -2,19 +2,32 @@ var dindin = 0;
 var dindinperclick = 1;
 var upgradeclick = 10;
 
+// if (dindin < upgradeclick) {
+//   document.getElementById("potencia").disabled = true;
+// } else {
+//   document.getElementById("potencia").disabled = false;
+// }
+
+// Aumentar dinheiro a cada clique
 function dinheiro() {
   dindin = dindin + dindinperclick;
   document.getElementById("dinheiro").textContent = dindin;
 }
+// Aumentar clique ao comprar upgrade
 function moredinheiro() {
-  if (dindin < 10) {
-    // document.getElementById("potencia").disabled = true;
+  if (dindin < upgradeclick) {
     alert("você não possui dinheiro");
   } else {
-    dindin = dindin - 10;
-    dindinperclick = Math.round(dindinperclick * 1.25 * 100) / 100;
-    upgradeclick = Math.round(upgradeclick * 1.45 * 100) / 100;
-    document.getElementById("dinheiro").textContent = dindin;
-    document.getElementById("potencia").textContent = upgradeclick;
+    if (dindinperclick == 1) {
+      dindin = dindin - 10;
+      dindinperclick = 5;
+      upgradeclick = 15;
+    } else {
+      dindin = dindin - upgradeclick;
+      dindinperclick = Math.round(dindinperclick * 1.25);
+      upgradeclick = Math.round(upgradeclick * 2);
+    }
   }
+  document.getElementById("dinheiro").textContent = dindin;
+  document.getElementById("potencia").textContent = upgradeclick;
 }
