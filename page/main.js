75142f5e5cab41ef13a47ea46e574712e71f0230
NicolasPietroDEV <99685174+NicolasPietroDEV@users.noreply.github.com>
2022-03-16 15:41:02 -0300

    Versão 0.5 Beta (Protótipo Automático)

--- page/main.js
+++ page/main.js
@@ -1,6 +1,17 @@
 var dindin = 0;
+
 var dindinperclick = 1;
 var upgradeclick = 10;
+
+var farmaciaprice = 7500;
+var acougueprice = 15000;
+var superprice = 30000;
+var shopprice = 60000;
+
+var farmaciagain = 10;
+var acouguegain = 50;
+var supergain = 100;
+var shopgain = 300;
 
 // if (dindin < upgradeclick) {
 //   document.getElementById("potencia").disabled = true;
@@ -14,9 +25,9 @@ function dinheiro() {
   document.getElementById("dinheiro").textContent = dindin;
 }
 // Aumentar clique ao comprar upgrade
-function moredinheiro() {
+function moredinheiro(porcganho, porcpreco) {
   if (dindin < upgradeclick) {
-    alert("você não possui dinheiro");
+    alert("você não tem grana, pobre, vai trabalhar");
   } else {
     if (dindinperclick == 1) {
       dindin = dindin - 10;
@@ -24,10 +35,11 @@ function moredinheiro() {
       upgradeclick = 15;
     } else {
       dindin = dindin - upgradeclick;
-      dindinperclick = Math.round(dindinperclick * 1.25);
-      upgradeclick = Math.round(upgradeclick * 2);
+      dindinperclick = Math.round(dindinperclick * porcganho);
+      upgradeclick = Math.round(upgradeclick * porcpreco);
     }
   }
   document.getElementById("dinheiro").textContent = dindin;
   document.getElementById("potencia").textContent = upgradeclick;
+  document.getElementById("dpc").textContent = dindinperclick;
 }
