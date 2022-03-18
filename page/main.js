91bf0d4798c57a10620aeeb0a5582e6a60c8415a
NicolasPietroDEV <99685174+NicolasPietroDEV@users.noreply.github.com>
2022-03-17 21:38:13 -0300

    Versão 0.7 Beta (Automaticos adicionados)

--- page/main.js
+++ page/main.js
@@ -3,23 +3,21 @@ var dindin = 0;
 var dindinperclick = 1;
 var upgradeclick = 10;
 
-var farmaciaprice = 7500;
-var acougueprice = 15000;
-var superprice = 30000;
-var shopprice = 60000;
+var farmaciaprice = 0;
+var farmaciagain = 0;
+var acougueprice = 0;
+var acouguegain = 0;
+var superprice = 0;
+var supergain = 0;
+var shopprice = 0;
+var shopgain = 0;
 
-var farmaciagain = 10;
-var acouguegain = 50;
-var supergain = 100;
-var shopgain = 300;
+var cps;
 
-// if (dindin < upgradeclick) {
-//   document.getElementById("potencia").disabled = true;
-// } else {
-//   document.getElementById("potencia").disabled = false;
-// }
+function teste() {
+  document.getElementById("farmacias").style.display = "none";
+}
 
-// Aumentar dinheiro a cada clique
 function dinheiro() {
   dindin = dindin + dindinperclick;
   document.getElementById("dinheiro").textContent = dindin;
@@ -31,7 +29,7 @@ function moredinheiro(porcganho, porcpreco) {
   } else {
     if (dindinperclick == 1) {
       dindin = dindin - 10;
-      dindinperclick = 5;
+      dindinperclick = 100000000;
       upgradeclick = 15;
     } else {
       dindin = dindin - upgradeclick;
@@ -43,3 +41,78 @@ function moredinheiro(porcganho, porcpreco) {
   document.getElementById("potencia").textContent = upgradeclick;
   document.getElementById("dpc").textContent = dindinperclick;
 }
+
+function farma() {
+  if (dindin >= farmaciaprice) {
+    if (farmaciaprice == 0) {
+      0;
+      farmaciaprice = 7500;
+      farmaciagain = 10;
+    } else {
+      dindin = dindin - farmaciaprice;
+      farmaciaprice = farmaciaprice * 2;
+      farmaciagain = farmaciagain * 1.25;
+      console.log(farmaciaprice, dindin);
+    }
+    document.getElementById("farmacia").textContent = farmaciaprice;
+    document.getElementById("farmaciadps").textContent = farmaciagain;
+    document.getElementById("dinheiro").textContent = dindin;
+  }
+}
+
+function acougue() {
+  if (dindin >= acougueprice) {
+    if (acougueprice == 0) {
+      acougueprice = 15000;
+      acouguegain = 50;
+    } else {
+      dindin = dindin - acougueprice;
+      acougueprice = acougueprice * 2;
+      acouguegain = acouguegain * 1.25;
+    }
+    document.getElementById("acougue").textContent = acougueprice;
+    document.getElementById("acouguedps").textContent = acouguegain;
+    document.getElementById("dinheiro").textContent = dindin;
+  }
+}
+
+function superm() {
+  if (dindin >= superprice) {
+    if (superprice == 0) {
+      superprice = 30000;
+      supergain = 100;
+    } else {
+      dindin = dindin - superprice;
+      superprice = superprice * 2;
+      supergain = supergain * 1.25;
+    }
+    document.getElementById("super").textContent = superprice;
+    document.getElementById("superdps").textContent = supergain;
+    document.getElementById("dinheiro").textContent = dindin;
+  }
+}
+
+function shop() {
+  if (dindin >= shopprice) {
+    if (shopprice == 0) {
+      shopprice = 60000;
+      shopgain = 300;
+    } else {
+      dindin = dindin - shopprice;
+      shopprice = shopprice * 2;
+      shopgain = shopgain * 1.25;
+    }
+    document.getElementById("shop").textContent = shopprice;
+    document.getElementById("shopdps").textContent = shopgain;
+    document.getElementById("dinheiro").textContent = dindin;
+  }
+}
+
+function cps() {
+  cps = farmaciagain + acouguegain + supergain + shopgain;
+  dindin = dindin + cps;
+  document.getElementById("dinheiro").textContent = dindin;
+  console.log("hello");
+}
+
+setInterval(cps, 1000);
